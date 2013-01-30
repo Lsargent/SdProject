496f19411a153013c5be60cf30d2df68652e47b6
Barton <bsargen3@uwyo.edu>
2013-01-30 12:50:46 -0700

    Added DataAccess. House, SdDb, and Address classes.

---
+++ DataAccess/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class House
+    {
+        public int id { get; set; }
+        public string HeatingType { get; set; }
+        public int StoryCount { get; set; }
+        public int RoomCount { get; set; }
+        public double FloorSpace { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
---
+++ DataAccess/SdDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class SdDb : DbContext 
+    {
+        public DbSet<House> Houses { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
